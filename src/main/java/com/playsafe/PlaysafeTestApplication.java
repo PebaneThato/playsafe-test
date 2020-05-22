938f51b6516e89efa1daae4c28e450fa6d83ed92
Thato Pebane <jpebane@gmail.com>
2020-05-22 03:48:38 +0200

    change default package to com.playsafe

--- src/main/java/com/playsafe/PlaysafeTestApplication.java
+++ src/main/java/com/playsafe/PlaysafeTestApplication.java
@@ -1,4 +1,4 @@
-package com.playsafe.playsafetest;
+package com.playsafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/playsafe/PlaysafeTestApplicationTests.java
+++ src/test/java/com/playsafe/PlaysafeTestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.playsafe.playsafetest;
+package com.playsafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
