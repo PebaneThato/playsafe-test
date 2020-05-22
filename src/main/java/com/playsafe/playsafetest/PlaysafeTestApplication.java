47bf6f492c642acb626e97f1a9c37a66919e185c
Thato Pebane <jpebane@gmail.com>
2020-05-22 02:10:48 +0200

    Application files generated using spring boot initializer

---
+++ src/main/java/com/playsafe/playsafetest/PlaysafeTestApplication.java
@@ -0,0 +1,13 @@
+package com.playsafe.playsafetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaysafeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaysafeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playsafe/playsafetest/PlaysafeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafe.playsafetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaysafeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
