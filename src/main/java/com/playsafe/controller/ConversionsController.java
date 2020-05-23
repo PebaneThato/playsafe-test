7b13f446c3e5db2f08ea87d2dfa2d1d396002881
Thato Pebane <jpebane@gmail.com>
2020-05-23 16:29:53 +0200

    Endpoints execution duration added to console logging

--- src/main/java/com/playsafe/controller/ConversionsController.java
+++ src/main/java/com/playsafe/controller/ConversionsController.java
@@ -3,6 +3,8 @@ package com.playsafe.controller;
 import com.playsafe.dto.DistanceDto;
 import com.playsafe.dto.TemperatureDto;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,27 +14,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/conversions")
 public class ConversionsController {
 
+    Logger logger = LoggerFactory.getLogger(ConversionsController.class);
+
     @PostMapping(path = "/ktoc/{temperatureInKelvin}")
     public TemperatureDto kelvinToCelsius(@PathVariable(value = "temperatureInKelvin") String temperatureInKelvin) {
+        long startTime = System.currentTimeMillis();
         double temperature = kelvinToCelsius(Double.parseDouble(temperatureInKelvin));
+        logger.info("\nExecution Time for ktoc: {} ms ", System.currentTimeMillis() - startTime);
         return new TemperatureDto(temperature);
     }
 
     @PostMapping(path = "/ctok/{temperatureInCelsius}")
     public TemperatureDto celsiusToKelvin(@PathVariable(value = "temperatureInCelsius") String temperatureInCelsius) {
+        long startTime = System.currentTimeMillis();
         double temperature = celsiusToKelvin(Double.parseDouble(temperatureInCelsius));
+        logger.info("\nExecution Time for ctoc: {} ms ", System.currentTimeMillis() - startTime);
         return new TemperatureDto(temperature);
     }
 
     @PostMapping(path = "/mtok/{distanceInMiles}")
     public DistanceDto milesToKilometers(@PathVariable(value = "distanceInMiles") String distanceInMiles) {
+        long startTime = System.currentTimeMillis();
         double distance = milesToKilometers(Double.parseDouble(distanceInMiles));
+        logger.info("\nExecution Time for mtok: {} ms ", System.currentTimeMillis() - startTime);
         return new DistanceDto(distance);
     }
 
     @PostMapping(path = "/ktom/{distanceInKilometers}")
     public DistanceDto kilometersToMiles(@PathVariable(value = "distanceInKilometers") String distanceInKilometers) {
+        long startTime = System.currentTimeMillis();
         double distance = kilometersToMiles(Double.parseDouble(distanceInKilometers));
+        logger.info("\nExecution Time for ktom: {} ms ", System.currentTimeMillis() - startTime);
         return new DistanceDto(distance);
     }
 
